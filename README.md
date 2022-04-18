d3cb49759a5a3a6701403d52fc6ffc5010d25380
calebtuttle <1calebtuttle@gmail.com>
2022-04-18 08:44:29 -0400

    Add to README and fix getHolo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WTF-Cache
 A simple server-side cache for data stored on WTF Protocol smart contracts. The data in this cache is exposed via API endpoints.
+
+Endpoints:
+- /getAllUserAddresses
+- /getHolo?address=<crypto_address>
+
--- src/services/getHolo.service.js
+++ src/services/getHolo.service.js
@@ -6,12 +6,12 @@ module.exports = {
   getHolo: async (req, res) => {
     // The key-value pairing for a cached holo looks like this:
     // {holo<address> : object_with_same_shape_as_return_value_of_getHolo_in_wtf-lib}
-    let userHolo = cache.get(`holo${address}`)
+    let userHolo = cache.get(`holo${req.query.address}`)
     if (userHolo) {
       return userHolo
     }
-    userHolo = await wtf.getHolo(address)
-    let success = cache.set(`holo${address}`, userHolo, 300) // 300s == delete from cache after 5 minutes
+    userHolo = await wtf.getHolo(req.query.address)
+    let success = cache.set(`holo${req.query.address}`, userHolo, 300) // 300s == delete from cache after 5 minutes
     return res.status(200).json({ holo: userHolo })
   }
 }
